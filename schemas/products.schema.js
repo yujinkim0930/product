8b06fc4256cf13edc10b72abf5b373bdc566fc7f
kim yujin <yujin3009@naver.com>
2024-01-23 15:39:17 +0900

    스키마 수정

--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -1,23 +1,30 @@
 import mongoose from "mongoose";
 
-const productSchma = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const productSchma = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["FOR_SALE", "SOLD_OUT"],
+      default: "FOR_SALE",
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: Number,
-    required: true,
-  },
-  status: { type: String, enum: ["FOR_SALE", "SOLD_OUT"], default: "FOR_SALE" },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Prod", productSchma);
